766f3af5fa5833dfd31d4b417e8df0e6d69e2deb
Roberto <rmartin.martin@gmail.com>
2014-04-21 22:48:45 -0400

    adding jquery before bootstrap.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.min
 //= require jquery
+//= require bootstrap.min
 //= require jquery_ujs
 //= require forem
 //= require_directory .
